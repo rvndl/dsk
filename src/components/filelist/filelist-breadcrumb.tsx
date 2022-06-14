f33816f3bcbf2acba78a9d55469488b745f33f89
rvndl <rivendelltolump@gmail.com>
2022-06-15 01:47:10 +0200

    add breadcrumb and file icons

--- src/components/filelist/filelist-breadcrumb.tsx
+++ src/components/filelist/filelist-breadcrumb.tsx
@@ -15,7 +15,7 @@ export const FileListBreadcrumb = ({ path }: Props) => {
     <div className="flex px-4 py-1 my-2 border rounded-lg border-slate-200">
       <div className="text-slate-400">/</div>
       {directories.map((dir) => (
-        <div className="flex">
+        <div className="flex" key={dir}>
           <p className="px-1 mx-1 text-blue-700 bg-blue-500 rounded-lg bg-opacity-20">
             {dir}
           </p>
